e006a7c641597c0e7c7671c36fd3366e2adc7a0f
hateteen <116436850+hateteen@users.noreply.github.com>
2022-12-09 16:52:09 +0300

    z50

--- Program.cs
+++ Program.cs
@@ -1,35 +1,86 @@
 ﻿void Zadacha47()
+{
+    Console.WriteLine("введите количество строк");
+    int linesVol = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите количество столбцов");
+    int columnsVol = Convert.ToInt32(Console.ReadLine());
+    double[,] numbers = new double[linesVol, columnsVol];
+    FillArrayRandomNumbers(numbers);
+    PrintArray(numbers);
+
+    void FillArrayRandomNumbers(double[,] array)
     {
-        Console.WriteLine("введите количество строк");
-        int linesVol = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("введите количество столбцов");
-        int columnsVol = Convert.ToInt32(Console.ReadLine());
-        double[,] numbers = new double[linesVol, columnsVol];
-        FillArrayRandomNumbers(numbers);
-        PrintArray(numbers);
-
-        void FillArrayRandomNumbers(double[,] array)
+        for (int i = 0; i < array.GetLength(0); i++)
         {
-            for (int i = 0; i < array.GetLength(0); i++)
+             for (int j = 0; j < array.GetLength(1); j++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
-                }
-            }
-        }
-
-        void PrintArray(double[,] array)
-        {
-            for (int i = 0; i < array.GetLength(0); i++)
-            {
-                Console.Write("[ ");
-                for (int j = 0; j < array.GetLength(1); j++)
-                {
-                    Console.Write(array[i, j] + " ");
-                }
-                Console.Write("]");
-                Console.WriteLine("");
+                array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
             }
         }
     }
+
+    void PrintArray(double[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("[ ");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.Write("]");
+             Console.WriteLine("");
+        }
+    }   
+}
+
+void Zadacha50()
+{
+    Console.WriteLine("введите номер строки");
+    int n = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("введите номер столбца");
+    int m = Convert.ToInt32(Console.ReadLine());
+    int [,] numbers = new int [10,10];
+    FillArrayRandomNumbers(numbers);
+
+    if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+    }
+
+    PrintArray(numbers);
+
+    void FillArrayRandomNumbers(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+            {        
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array [i,j] = new Random().Next(-100, 100)/10;
+                }   
+            }
+    }
+
+    void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("[ ");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i,j] + " ");
+            }   
+            Console.Write("]");
+            Console.WriteLine(""); 
+        }
+    }
+}
+
+void Zadacha52()
+{
+    
+}
